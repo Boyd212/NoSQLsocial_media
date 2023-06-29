1961ee9ba3c73903fd0d542ce714aeab64af0c79
unknown <matthewfive.30@gmail.com>
2023-06-29 16:24:31 -0500

    Add models 2

---
+++ models/Comment.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 200,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id:false,
+    }
+);
+
+const CommentSchema = new Schema(
+    {
+        commentText: {
+            type: String,
+            required: "Comment Required",
+            minlength: 1,
+            maxlength: 200,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+CommentSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Comment = model("Comment", CommentSchema);
+
+module.exports = Comment;
